9a21313743e8fb918b543d4936647bba96a6501e
Kingravan <49265172+Kingravan@users.noreply.github.com>
2022-03-21 22:02:06 +0530

    Add files via upload

---
+++ js/instagram_script.js
@@ -0,0 +1,35 @@
+function Home()
+{
+    window.location.href = "../index.html";
+}
+function Youtube()
+{
+    window.location.href = "../html/youtube.html";
+}
+function Instagram()
+{
+    document.getElementById("Home").style.backgroundColor = "transparent";
+    document.getElementById("Home").style.color = "black";
+    document.getElementById("Youtube").style.backgroundColor = "transparent";
+    document.getElementById("Youtube").style.color = "black";
+    document.getElementById("Instagram").style.backgroundColor = "blue";
+    document.getElementById("Instagram").style.color = "white";
+    document.getElementById("Facebook").style.backgroundColor = "transparent";
+    document.getElementById("Facebook").style.color = "black";
+    document.getElementById("Snapchat").style.backgroundColor = "transparent";
+    document.getElementById("Snapchat").style.color = "black";
+    document.getElementById("WhatsApp").style.backgroundColor = "transparent";
+    document.getElementById("WhatsApp").style.color = "black";
+}
+function Facebook()
+{
+    window.location.href = "../html/facebook.html";
+}
+function Snapchat()
+{
+    window.location.href = "../html/snapchat.html";
+}
+function WhatsApp()
+{
+    window.location.href = "../html/whatsapp.html";
+}
---
+++ js/snapchat_script.js
@@ -0,0 +1,35 @@
+function Home()
+{
+    window.location.href = "../index.html";
+}
+function Youtube()
+{
+    window.location.href = "../html/youtube.html";
+}
+function Instagram()
+{
+    window.location.href = "../html/instagram.html";
+}
+function Facebook()
+{
+    window.location.href = "../html/facebook.html";
+}
+function Snapchat()
+{
+    document.getElementById("Home").style.backgroundColor = "transparent";
+    document.getElementById("Home").style.color = "black";
+    document.getElementById("Youtube").style.backgroundColor = "transparent";
+    document.getElementById("Youtube").style.color = "black";
+    document.getElementById("Instagram").style.backgroundColor = "transparent";
+    document.getElementById("Instagram").style.color = "black";
+    document.getElementById("Facebook").style.backgroundColor = "transparent";
+    document.getElementById("Facebook").style.color = "black";
+    document.getElementById("Snapchat").style.backgroundColor = "blue";
+    document.getElementById("Snapchat").style.color = "white";
+    document.getElementById("WhatsApp").style.backgroundColor = "transparent";
+    document.getElementById("WhatsApp").style.color = "black";
+}
+function WhatsApp()
+{
+    window.location.href = "../html/whatsapp.html";
+}
